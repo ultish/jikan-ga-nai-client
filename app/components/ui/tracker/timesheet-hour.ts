922ad3c65c281fed9bfb0d24d7dc0b1a9e0398ef
Jimmy <ultish@gmail.com>
2020-06-02 17:31:59 +1000

    timesheet totals display

--- app/components/ui/tracker/timesheet-hour.ts
+++ app/components/ui/tracker/timesheet-hour.ts
@@ -2,6 +2,7 @@ import Component from "@glimmer/component";
 
 interface UiTrackedTimesheetHourArgs {
   value: number;
+  isTotal: boolean;
 }
 
 export default class UiTrackedTimesheet extends Component<
@@ -12,14 +13,21 @@ export default class UiTrackedTimesheet extends Component<
   }
 
   get display() {
-    let result = this.args.value / 60;
+    let result = this.args.value;
+    if (!this.args.isTotal) {
+      result = result / 60;
+    }
+
     result = parseFloat(result.toFixed(1));
 
     return result;
   }
 
   get displayDetailed() {
-    let result = this.args.value / 60;
+    let result = this.args.value;
+    if (!this.args.isTotal) {
+      result = result / 60;
+    }
     return parseFloat(result.toFixed(3));
   }
 }
